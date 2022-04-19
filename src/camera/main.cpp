3e1741fbb867b42a971aa5588cd34b09072763ca
l-pires <pereirapireslucas@gmail.com>
2022-04-19 12:18:38 -0300

    Add temporização, threads e IPC corretamente

--- src/camera/main.cpp
+++ src/camera/main.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <arpa/inet.h>
+
 #include <opencv2/imgcodecs.hpp>
 #include "opencv2/core/core.hpp"
 #include <opencv2/core/types_c.h>
@@ -5,14 +16,34 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/videoio/videoio_c.h>
-#include <iostream>
-#include <unistd.h>
 
 using namespace cv;
 using namespace std;
 
-int main()
-{
+typedef struct {
+    Mat data;
+    bool ready;
+} tparams;
+
+bool capture = false;
+char socket_name[100];
+int  socket_id;
+
+void timer(int signum) {
+    capture = true;
+}
+
+void end(int signum) {
+    printf("\nFechando o programa.\n");
+    unlink(socket_name);
+	close(socket_id);
+    exit(signum);
+}
+
+void *capture_thread(void *pparams) {
+    tparams *params;
+    params = (tparams*) pparams;
+
     // A imagem captada pela webcam -> adaptar para receber a imagem da câmera rpi.
     VideoCapture cap(0);
     // Iniciando a imagem.
@@ -26,11 +57,14 @@ int main()
     if (detectorCascade.empty())
     {
         cout << "Arquivo XML não carregado" << endl;
-        return -1;
+        exit(-1);
     }
 
     while (1)
     {
+        while(!capture);
+        capture = false;
+
         // Capturar imagem da webcam -> adaptar para receber a imagem da câmera rpi.
         cap.read(img);
         // Convertendo a escala de cor.
@@ -46,9 +80,66 @@ int main()
             rectangle(img, pessoa[i].tl(), pessoa[i].br(), Scalar(255, 0, 255), 2);
         }
 
-        imwrite("../image.jpeg", img);
-        usleep(40e3);
+        params->data = img.clone();
+        params->ready = true;
     }
+}
 
+void *send_thread(void *pparams) {
+    tparams *params;
+    params = (tparams*) pparams;
+
+    struct sockaddr socket_struct;
+	strcpy(socket_name, "/tmp/video");
+	socket_id = socket(PF_LOCAL, SOCK_STREAM, 0);
+	socket_struct.sa_family = AF_LOCAL;
+	strcpy(socket_struct.sa_data, socket_name);
+	bind(socket_id, &socket_struct, sizeof(socket_struct));
+
+	listen(socket_id, 10);
+	while(1)
+	{
+		int socket_id_cliente, end_server_ok;
+		struct sockaddr clienteAddr;
+		socklen_t clienteLength = sizeof( (struct sockaddr *) &clienteAddr);
+		socket_id_cliente = accept(socket_id, &clienteAddr, &clienteLength);
+
+        while(1) {
+            while(!params->ready);
+            params->ready = false;
+
+            // int size = params->data.total() * params->data.channels();
+            // vector<unsigned char> buffer;
+            // imencode(".jpeg", params->data, buffer);
+            // string data = base64_encode(buffer.data(), buffer.size());
+            // write(socket_id_cliente, data.c_str(), data.length());
+
+            imwrite("../image.jpeg", params->data);
+            write(socket_id_cliente, ".", 2);
+        }
+
+		close(socket_id_cliente);
+	}
+    end(0);
+}
+
+int main()
+{
+    signal(SIGINT, end);
+    signal(SIGALRM, timer);
+    ualarm(1000, 50e3);
+
+    tparams params;
+    params.ready = false;
+
+    pthread_t camera;
+    pthread_create(&camera, NULL, &capture_thread, &params);
+
+    pthread_t send;
+    pthread_create(&send, NULL, &send_thread, &params);
+
+    pthread_join(camera, NULL);
+    pthread_join(send, NULL);
+    
     return 0;
 }
--- src/server/index.js
+++ src/server/index.js
@@ -2,39 +2,33 @@ const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const fs = require('fs');
+const net = require('net');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const local = net.connect('/tmp/video');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-let user_count = 0;
-let streaming;
+let sockets = [];
 
 io.on('connection', (socket) => {
-  if(user_count === 0) {
-    streaming = setInterval(() => {
-      const data = fs.readFileSync('../picture/image.jpeg');
-      const buffer = Buffer.from(data).toString('base64');
-      if(buffer.length > 60e3) {
-        socket.broadcast.emit('image', buffer);
-      }
-    }, 40);
-  }
-
+  sockets.push(socket);
   console.log('User connected');
-  user_count++;
 
   socket.on('disconnect', () => {
+    sockets = sockets.filter(s => s !== socket);
     console.log('User disconnected');
-    user_count--;
-    if(user_count == 0) {
-      clearInterval(streaming);
-    }
   });
+});
+
+local.on('data', _ => {
+  const data = fs.readFileSync('../camera/image.jpeg');
+  const buffer = Buffer.from(data).toString('base64');
+  io.emit('image', buffer);
 });
 
 server.listen(3000, () => {
