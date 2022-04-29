999a1752c46c190d4a1ad22e659400b228736d09
Gustavo Oliveira <gustavomont97@gmail.com>
2022-04-28 21:43:04 -0300

    add descritor de recursos

--- src/camera/main.cpp
+++ src/camera/main.cpp
@@ -20,29 +20,33 @@
 using namespace cv;
 using namespace std;
 
-typedef struct {
+typedef struct
+{
     Mat data;
     bool ready;
 } tparams;
 
 bool capture = false;
 char socket_name[100];
-int  socket_id;
+int socket_id;
 
-void timer(int signum) {
+void timer(int signum)
+{
     capture = true;
 }
 
-void end(int signum) {
+void end(int signum)
+{
     printf("\nFechando o programa.\n");
     unlink(socket_name);
-	close(socket_id);
+    close(socket_id);
     exit(signum);
 }
 
-void *capture_thread(void *pparams) {
+void *capture_thread(void *pparams)
+{
     tparams *params;
-    params = (tparams*) pparams;
+    params = (tparams *)pparams;
 
     // A imagem captada pela webcam -> adaptar para receber a imagem da câmera rpi.
     VideoCapture cap(0);
@@ -50,19 +54,23 @@ void *capture_thread(void *pparams) {
     Mat img, gray;
 
     // Carregando modelo de detecção corporal/rosto.
-    CascadeClassifier detectorCascade;
-    detectorCascade.load("../models/haarcascade_fullbody.xml");
+    cv::HOGDescriptor hog;
+    hog.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+
+    // CascadeClassifier detectorCascade;
+    // detectorCascade.load("../models/haarcascade_fullbody.xml");
 
     // Mensagem de erro caso haja erro na abertura do modelo de detecção.
-    if (detectorCascade.empty())
-    {
-        cout << "Arquivo XML não carregado" << endl;
-        exit(-1);
-    }
+    // if (detectorCascade.empty())
+    //{
+    //    cout << "Arquivo XML não carregado" << endl;
+    //    exit(-1);
+    //}
 
     while (1)
     {
-        while(!capture);
+        while (!capture)
+            ;
         capture = false;
 
         // Capturar imagem da webcam -> adaptar para receber a imagem da câmera rpi.
@@ -70,11 +78,17 @@ void *capture_thread(void *pparams) {
         // Convertendo a escala de cor.
         cvtColor(img, gray, cv::COLOR_BGR2GRAY);
         // Criando um vetor para armazenar a pessoa detectada em um retângulo.
+        // std::vector<cv::Rect> pessoa;
         vector<Rect> pessoa;
-        // Parâmetros de detecção.
-        detectorCascade.detectMultiScale(gray, pessoa, 1.1, 5, cv::CASCADE_SCALE_IMAGE, Size(30, 30));
+        //  Parâmetros de detecção.
+        hog.detectMultiScale(gray, pessoa, 0, cv::Size(8, 8), cv::Size(32, 32), 1.2, 2);
+        // hog.detectMultiScale(gray, pessoa, 1.1, 5, cv::CASCADE_SCALE_IMAGE, Size(30, 30));
 
         // Criando o retângulo na pessoa.
+        // for (int i = 0; i < pessoa.size(); i++)
+        //{
+        //    rectangle(img, pessoa[i].tl(), pessoa[i].br(), Scalar(255, 0, 255), 2);
+        //}
         for (int i = 0; i < pessoa.size(); i++)
         {
             rectangle(img, pessoa[i].tl(), pessoa[i].br(), Scalar(255, 0, 255), 2);
@@ -85,27 +99,30 @@ void *capture_thread(void *pparams) {
     }
 }
 
-void *send_thread(void *pparams) {
+void *send_thread(void *pparams)
+{
     tparams *params;
-    params = (tparams*) pparams;
+    params = (tparams *)pparams;
 
     struct sockaddr socket_struct;
-	strcpy(socket_name, "/tmp/video");
-	socket_id = socket(PF_LOCAL, SOCK_STREAM, 0);
-	socket_struct.sa_family = AF_LOCAL;
-	strcpy(socket_struct.sa_data, socket_name);
-	bind(socket_id, &socket_struct, sizeof(socket_struct));
+    strcpy(socket_name, "/tmp/video");
+    socket_id = socket(PF_LOCAL, SOCK_STREAM, 0);
+    socket_struct.sa_family = AF_LOCAL;
+    strcpy(socket_struct.sa_data, socket_name);
+    bind(socket_id, &socket_struct, sizeof(socket_struct));
 
-	listen(socket_id, 10);
-	while(1)
-	{
-		int socket_id_cliente, end_server_ok;
-		struct sockaddr clienteAddr;
-		socklen_t clienteLength = sizeof( (struct sockaddr *) &clienteAddr);
-		socket_id_cliente = accept(socket_id, &clienteAddr, &clienteLength);
+    listen(socket_id, 10);
+    while (1)
+    {
+        int socket_id_cliente, end_server_ok;
+        struct sockaddr clienteAddr;
+        socklen_t clienteLength = sizeof((struct sockaddr *)&clienteAddr);
+        socket_id_cliente = accept(socket_id, &clienteAddr, &clienteLength);
 
-        while(1) {
-            while(!params->ready);
+        while (1)
+        {
+            while (!params->ready)
+                ;
             params->ready = false;
 
             // int size = params->data.total() * params->data.channels();
@@ -118,8 +135,8 @@ void *send_thread(void *pparams) {
             write(socket_id_cliente, ".", 2);
         }
 
-		close(socket_id_cliente);
-	}
+        close(socket_id_cliente);
+    }
     end(0);
 }
 
@@ -140,6 +157,6 @@ int main()
 
     pthread_join(camera, NULL);
     pthread_join(send, NULL);
-    
+
     return 0;
 }
